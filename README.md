a49136580d67cbef9be629c00286ff5eeb6ffc36
Henry <pye.henry@protonmail.com>
2022-03-11 22:20:06 +0100

    First commit

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # FA2
 
-#### Install the CLI (TypeScript):
+à prendre -> [TristanNCl](https://github.com/TristanNcl)
+
+#### Install the CLI (TypeScript)
 
 To install all the dependencies of the project please run:
 
 ```
-$ cd /smart-contract-helper
-$ yarn install
-$ yarn build
-$ yarn install -g
-```
-In order to run the tests:
-```
-$ yarn run-script test
+cd /smart-contract-helper
+yarn install
+yarn build
+yarn install -g
 ```
 
+In order to run the tests:
+
+```
+yarn run-script test
+```
--- src/fa2.helper.ts
+++ src/fa2.helper.ts
@@ -1,19 +1,31 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-import * as ver from './ver';
-import * as contract from './contract';
+const program = require("commander");
+import * as ver from "./ver";
+import * as contract from "./contract";
+
+program.command("compile-contract").action(contract.compileContract);
+
+program.command("deploy-contract").action(contract.deployContract);
 
 program
-    .command('compile-contract')
-    .action(contract.compileContract)
+  .command("hex-encode")
+  .arguments("<string> string to parse to hex")
+  .action((str) => {
+    const hexCode = Buffer.from(str, "utf8").toString("hex");
+
+    console.log(hexCode);
+  });
 
 program
-    .command('deploy-contract')
-    .action(contract.deployContract)
+  .command("hex-decode")
+  .arguments("<string> string to parse from hex")
+  .action((str) => {
+    const stringCode = Buffer.from(str, "hex").toString("utf8");
 
-program
-    .option('-v', 'show version', ver, '')
-    .action(ver.showVersion);
+    console.log(stringCode);
+  });
 
-program.parse(process.argv)
+program.option("-v", "show version", ver, "").action(ver.showVersion);
+
+program.parse(process.argv);
